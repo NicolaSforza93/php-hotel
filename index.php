842fea271e3bacfad7d5998c8eb2d413502df5bc
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-22 16:10:35 +0100

    printed hotel data on the page

--- index.php
+++ index.php
@@ -8,6 +8,78 @@
 </head>
 
 <body>
+    <?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+    ?>
+    <ul>
+        <?php
+        foreach ($hotels as $hotel) {
+            foreach ($hotel as $key => $value) {
+        ?>
+                <li>
+                    <?php echo $key; ?> : <?php echo $value ?>
+                </li>
+        <?php
+            }
+        }
+
+        ?>
+    </ul>
+
+    <!-- <ul>
+        <?php
+        for ($i = 0; $i < count($hotels); $i++) {
+            foreach ($hotels[$i] as $key => $value) {
+        ?>
+                <li>
+                    <?php echo $key; ?> : <?php echo $value ?>
+                </li>
+        <?php
+            }
+        }
+
+        ?>
+    </ul> -->
+
 
 </body>
 
